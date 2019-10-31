55c55b8bde0a7b7fff5fd8ea921914561dc9825a
ALFIAN SATRIA V.P <masfian77@gmail.com>
2019-10-31 17:14:00 +0700

    committed

---
+++ app/src/main/java/com/alfian/recyclerview/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.alfian.recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val list = ArrayList<Hero>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rv_heroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+
+    }
+
+    fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val listHero = ArrayList<Hero>()
+        for (position in dataName.indices) {
+            val hero = Hero(
+                dataName[position],
+                dataDescription[position],
+                dataPhoto[position]
+            )
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        rv_heroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rv_heroes.adapter = listHeroAdapter
+    }
+}
